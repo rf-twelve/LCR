238f583c1fe7f076c2b88ebab582cce1209ab922
Rosel Francisco <francisco12rosel@gmail.com>
2023-03-24 01:49:07 +0800

    Added new folder

---
+++ app/Http/Livewire/Dts/SharedDocuments.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Dts;
+
+use Livewire\Component;
+
+class SharedDocuments extends Component
+{
+    public function render()
+    {
+        return view('livewire.dts.shared-documents');
+    }
+}
